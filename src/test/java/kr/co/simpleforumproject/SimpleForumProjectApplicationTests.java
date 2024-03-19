3e31d4ebe907a08fd1d65518c8a625ec17314737
김경준 <kgj0314@hanyang.ac.kr>
2024-03-19 15:19:42 +0900

    update

---
+++ src/test/java/kr/co/simpleforumproject/SimpleForumProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.simpleforumproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleForumProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
